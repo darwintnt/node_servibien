b265986409da7992798e314869daf1e4ce4e6d8d
darwin.gomez@insoftar.com-TechLead <darwin.gomez@insoftar.com>
2023-08-16 11:22:50 -0500

    correccion index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import express from 'express'
+import morgan from "morgan";
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
+// Initialize express
 const app = express()
-
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+// settings
+app.set("port", process.env.PORT || 3000);
 app.set('views', join(__dirname, 'views'))
-
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
 
+// middlewares
+app.use(morgan("dev"));
+
+// static files
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(process.env.PORT || 3000)
-console.log('server is listening on port', process.env.PORT || 3000);
+// listening the Server
+app.listen(app.get("port"));
+console.log("server is listening on port", app.get("port"));
+
 
